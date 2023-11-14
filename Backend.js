b39b83fb473b748df97f5c38fe07133363730bdd
JEEVACSE1 <108452290+JEEVACSE1@users.noreply.github.com>
2023-11-14 14:24:48 +0530

    backend node.js
    Backend (Node.js with Express):
    
    
    Create a new directory for your project and navigate into it:
    
    mkdir react-node-mongodb-app
    cd react-node-mongodb-app
    
    
    Initialize the backend:
    
    npm init -y
    npm install express mongoose body-parser cors
    
    
    Create a file named server.js for your backend:

---
+++ Backend.js
@@ -0,0 +1,39 @@
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/your_database_name', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a simple schema for MongoDB
+const ItemSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model('Item', ItemSchema);
+
+// API endpoint to get all items
+app.get('/api/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
